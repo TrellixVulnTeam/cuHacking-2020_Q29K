010b6b95d9efb6efb5a630e3c1938ced0c704cb4
echu87 <echu875615@gmail.com>
2020-01-12 01:32:46 -0500

    added getters and setters for all attributes

--- backend/Analysis/scripts/model.py
+++ backend/Analysis/scripts/model.py
@@ -83,6 +83,21 @@ def load_model(model_type):
 def set_model_type(model_t):
     model_type = model_t
 
+def get_model_type():
+    return model_type
+
+def set_breakpoint(breakpoint):
+    break_point = breakpoint
+
+def get_breakpoint():
+    return break_point
+
+def set_sizedataset(size):
+    size_dataset = size
+
+def get_sizedataset():
+    return size_dataset
+
 set_model_type("sentiment_analysis")
 model = load_model(model_type)
 print(infer_model("I went and saw this movie last night after being coaxed to by a few friends of mine. I'll admit that I was reluctant to see it because from what I knew of Ashton Kutcher he was only able to do comedy. I was wrong. Kutcher played the character of Jake Fischer very well, and Kevin Costner played Ben Randall with such professionalism. The sign of a good movie is that it can toy with our emotions. This one did exactly that. The entire theater (which was sold out) was overcome by laughter during the first half of the movie, and were moved to tears during the second half. While exiting the theater I not only saw many women in tears, but many full grown men as well, trying desperately not to let anyone see them crying. This movie was great, and I suggest that you go see it before you judge.", model))
